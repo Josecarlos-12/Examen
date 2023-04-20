cf5a7ea07076d8c81c3056a93359ab59bdf75fef
Raky1305 <a.rb13@icloud.com>
2023-04-19 22:50:21 -0500

    Clase y IShowData : Player y Clase : NPC
    Alumno 1: Alvaro ramos

---
+++ Examen/Examen/NPC.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    class NPC
+    {
+        abstract class NPC : IShowData
+        {
+            protected string name;
+            protected float life;
+            protected string type;
+
+            protected NPC(string name, float life, string type)
+            {
+                this.name = name;
+                this.life = life;
+                this.type = type;
+
+            }
+        }
+    }
+}
---
+++ Examen/Examen/Player.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    class Player
+    {
+        protected string name;
+        protected string itemList;
+        protected float experience;
+        protected int level;
+        protected float money;
+
+        public Player(string name, string itemList, float experience, int level, float money)
+        {
+            this.name = name;
+            this.itemList = itemList;
+            this.experience = experience;
+            this.level = level;
+            this.money = money;
+        }
+
+        protected virtual string GetName()
+        {
+            return name;
+        }
+        protected virtual int GetLevel()
+        {
+            return level;
+        }
+
+        protected virtual float GetExperience()
+        {
+            return experience;
+        }
+        protected virtual string GetItemList()
+        {
+            return itemList;
+        }
+        protected virtual float GetMoney()
+        {
+            return money;
+        }
+        public string IShowData()
+        {
+            return $"Su nombre es{GetName()}, Su nivel es {GetLevel()}, su experiencia es {GetExperience()}, tiene con {GetMoney()} monedas y sus items son{GetItemList()}";
+        }
+    }
+}
