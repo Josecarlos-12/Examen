89bcef4712c3ae97cdaf436e4fe03d117fec2ce6
daniel13524 <danielenrique.saves@hotmail.com>
2023-04-19 23:04:24 -0500

    Clase Potions
    Alumno 6_Daniel Calderon

---
+++ Examen/Examen/Potions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    class Potions:Items,IShowData
+    {
+        protected int capacity;
+        public Potions(int capacity, string name, string type, float price) : base(name, type, price)
+        {
+            this.capacity = capacity;
+        }
+    }
+}
