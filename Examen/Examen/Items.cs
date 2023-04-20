2e12bcbb25707a3a35d1e1cd35483e763a4b33d2
CBADA <anthony.280697@gmail.com>
2023-04-19 22:41:24 -0500

    Classes: Item, Talkers
    Anthony Gutiérrez - Alumno 4

---
+++ Examen/Examen/Items.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    class Items : IShowData
+    {
+        protected string name;
+        protected string type;
+        protected float price;
+
+        protected Items(string name, string type, float price)
+        {
+            this.name = name;
+            this.type = type;
+            this.price = price;
+        }
+    }
+}
---
+++ Examen/Examen/Talkers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    class Talkers : NPC, IShowData
+    {
+        protected string textList;
+        public Talkers(string textList, string name, float life, string type) : base(name, life, type)
+        {
+            this.textList = textList;
+        }
+    }
+}
